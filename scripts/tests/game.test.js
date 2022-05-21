ad6bd33399c67dce97b7b73cb132c4e13f460b3e
lukaszkukla <lukaszkukla@gmail.com>
2022-05-21 12:45:01 +0000

    code along challenge 1 & 3

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,9 @@
 /**
  * @jest-environment jsdom
  */
-const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require('../game');
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require('../game');
+
+jest.spyOn(window, 'alert').mockImplementation(() => { });
 
 beforeAll(() => {
     let fs = require('fs');
@@ -17,7 +19,7 @@ describe('game object contains correct keys', () => {
     });
     test('currentGame key exists', () => {
         expect('currentGame' in game).toBe(true);
-    });    
+    });
     test('playerMoves key exists', () => {
         expect('playerMoves' in game).toBe(true);
     });
@@ -29,6 +31,15 @@ describe('game object contains correct keys', () => {
     });
     test('turnNumber key exists', () => {
         expect('turnNumber' in game).toBe(true);
+    });
+    test('lastButton key exists', () => {
+        expect('lastButton' in game).toBe(true);
+    });
+    test('turnInProgress key exists', () => {
+        expect('turnInProgress' in game).toBe(true);
+    });
+    test('turnInProgress default value is false', () => {
+        expect('turnInProgress' in game).toBe(true);
     });
 });
 
@@ -49,7 +60,7 @@ describe('newGame works correctly', () => {
     // test('should set currentGame to zero', () => {
     //     expect(game.currentGame.length).toBe(0);
     // });
-    
+
     // test('should set currentGame to zero', () => {
     //     expect(game.currentGame).toEqual([]);
     // });
@@ -71,7 +82,7 @@ describe('newGame works correctly', () => {
     });
     test('expect data-listener to be true', () => {
         const elements = document.getElementsByClassName('circle');
-        for(let element of elements) {
+        for (let element of elements) {
             expect(element.getAttribute('data-listener')).toEqual('true');
         }
     });
@@ -102,5 +113,26 @@ describe('gameplay works correctly', () => {
         game.turnNumber = 42;
         showTurns();
         expect(game.turnNumber).toBe(0);
+    });
+
+    test('should increment the score if the turn is correct', () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    })
+    test('should call an alert fi the move is wrong', () => {
+        game.playerMoves.push('wrong');
+        playerTurn();
+        expect(window.alert).toBeCalledWith('wrong move');
+    });
+    test('should toggle turnInProgress to true', () => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true);
+    });
+    test('clicking during computer sequence should fail', () => {
+        showTurns();
+        game.lastButton = '';
+        document.getElementById('button2').click();
+        expect(game.lastButton).toEqual('');
     });
 });
